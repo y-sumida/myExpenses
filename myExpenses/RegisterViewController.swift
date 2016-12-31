bb344951b31b815f3e8cfee435d1f910ef605a9d
Yuki Sumida <y.sumida.0201@gmail.com>
2016-12-31 14:34:36 +0900

    新規登録画面のパーツ配置

---
+++ myExpenses/RegisterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterViewController.swift
+//  myExpenses
+//
+//  Created by Yuki Sumida on 2016/12/31.
+//
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
