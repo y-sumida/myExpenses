43af53bb1e9fa4eaf7f78c80d51db05073cc210d
Yuki Sumida <y.sumida.0201@gmail.com>
2016-12-31 13:58:13 +0900

    ログイン画面のパーツ配置

--- myExpenses/LoginViewController.swift
+++ myExpenses/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  myExpenses
 //
 //  Created by Yuki Sumida on 2016/12/31.
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // ナビゲーションバー非表示
+        if let navi = navigationController {
+            navi.setNavigationBarHidden(true, animated: true)
+        }
     }
 
     override func didReceiveMemoryWarning() {
