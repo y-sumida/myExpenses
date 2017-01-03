29d499c0584e33fac79a0e6c7811d05b701ad347
Yuki Sumida <y.sumida.0201@gmail.com>
2017-01-03 16:10:02 +0900

    マッピング用とリクエスト用のプロトコル

--- myExpenses/LoginModel.swift
+++ myExpenses/LoginModel.swift
@@ -8,8 +8,39 @@
 import Foundation
 import RxSwift
 
-class LoginModel {
+public protocol RequestProtocol {
+    associatedtype Response
+    var request: NSMutableURLRequest {get}
+    func responseToObject(data: NSData) -> Response
+}
+
+public protocol ResponseProtocol {
+    init(data: NSDictionary)
+}
+
+class LoginModel: ResponseProtocol {
+    var success: Bool = false
+
+    required init(data: NSDictionary) {
+        if let success = data["success"] {
+            self.success = success as! Bool
+        }
+    }
+
     static func call(email: String, password: String) -> Observable<(NSDictionary, NSHTTPURLResponse)> {
+
+        let session: NSURLSession = NSURLSession.sharedSession()
+        let request: LoginRequest = LoginRequest(email: email, password: password)
+        return session.rx_responseObject(request.request)
+    }
+}
+
+class LoginRequest: RequestProtocol {
+    typealias Response = LoginModel
+    var email: String = ""
+    var password: String = ""
+
+    var request: NSMutableURLRequest {
         let body = NSMutableDictionary()
         body.setValue(email, forKey: "email");
         body.setValue(password, forKey: "password");
@@ -20,9 +51,20 @@ class LoginModel {
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("application/json", forHTTPHeaderField: "Accept")
         request.HTTPBody = try? NSJSONSerialization.dataWithJSONObject(body, options: NSJSONWritingOptions.init(rawValue: 2))
+        return request
+    }
 
-        let session: NSURLSession = NSURLSession.sharedSession()
+    init(email: String, password: String) {
+        self.email = email
+        self.password = password
+    }
 
-        return session.rx_responseObject(request)
+    func responseToObject(data: NSData) -> Response {
+        do {
+            let object = try NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers) as! NSDictionary
+            return Response(data: object)
+        } catch {
+            return Response(data: [:])
+        }
     }
 }
