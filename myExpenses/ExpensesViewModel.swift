284a1e8bd73467be44d3bc462c879cac82dfeb4c
Yuki Sumida <y.sumida.0201@gmail.com>
2017-01-07 23:28:24 +0900

    交通費一覧画面用のビューモデル仮実装

---
+++ myExpenses/ExpensesViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  ExpensesViewModel.swift
+//  myExpenses
+//
+//  Created by Yuki Sumida on 2017/01/07.
+//
+//
+
+import Foundation
+import RxSwift
+
+class ExpensesViewModel {
+    private let bag: DisposeBag = DisposeBag()
+
+    var period: Variable<String> = Variable("")
+    var fetchTrigger: PublishSubject<Void> = PublishSubject()
+    var result: Variable<ErrorType?> = Variable(nil)
+
+    init(sessionId: String) {
+        fetchTrigger
+            .flatMap {
+                ExpensesModel.call(sessionId, period: self.period.value)
+            }
+            .observeOn(MainScheduler.instance)//これ以降メインスレッドで実行
+            .subscribe(
+                onNext: { (model, response) in
+                    self.result.value = model.result!
+                    // TODO その他の項目
+                },
+                onError: { (error: ErrorType) in
+                    // APIエラー
+                    self.result.value = error
+                }
+            )
+            .addDisposableTo(bag)
+    }
+}
