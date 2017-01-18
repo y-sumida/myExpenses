6ae1b727cdadf7bcfe6e8fce1c94690c36aa8208
Yuki Sumida <y.sumida.0201@gmail.com>
2017-01-18 22:42:16 +0900

    交通費編集画面のスケルトン

---
+++ myExpenses/ExpenseEditViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ExpenseEditViewController.swift
+//  myExpenses
+//
+//  Created by Yuki Sumida on 2017/01/18.
+//
+//
+
+import UIKit
+
+class ExpenseEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
