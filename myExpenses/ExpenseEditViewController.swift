a593af4d89557b8aaf5cdcc2b713cc5cab345dae
Yuki Sumida <y.sumida.0201@gmail.com>
2017-01-19 23:12:52 +0900

    交通費編集画面のテーブル設定仮実装

--- myExpenses/ExpenseEditViewController.swift
+++ myExpenses/ExpenseEditViewController.swift
@@ -8,7 +8,34 @@
 
 import UIKit
 
-class ExpenseEditViewController: UIViewController {
+enum ExpenseEditSections: Int {
+    case Date = 0
+    case Destination
+    case Transport
+    case Interval
+    case Fare
+    case Memo
+
+    var title: String {
+        switch self {
+        case Date:
+            return "日付"
+        case Destination:
+            return "外出先"
+        case Transport:
+            return "交通機関"
+        case Interval:
+            return "区間"
+        case Fare:
+            return "料金"
+        case Memo:
+            return "備考"
+        }
+    }
+}
+
+class ExpenseEditViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+    @IBOutlet weak private var table: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,9 +45,30 @@ class ExpenseEditViewController: UIViewController {
             navigationItem.title = "外出先編集"
             navigationItem.hidesBackButton = false
         }
+
+        table.delegate = self
+        table.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // TODO　セクションごとのタイトル
+        return 6
+    }
+
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return ExpenseEditSections(rawValue: section)?.title
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //TODO セクションごとの行数
+        return 2
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
