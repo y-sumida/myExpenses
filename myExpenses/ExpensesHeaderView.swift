52c9a2887cecc0bc85ab8038038624e5ef261281
Yuki Sumida <y.sumida.0201@gmail.com>
2017-01-20 23:10:11 +0900

    ヘッダーのボタンタップで年月一覧のビューをPopover表示するお試し実装

--- myExpenses/ExpensesHeaderView.swift
+++ myExpenses/ExpensesHeaderView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ExpensesHeaderView: UIView {
+class ExpensesHeaderView: UIView, UIPopoverPresentationControllerDelegate{
     @IBOutlet weak var fareTotal: UILabel!
     @IBOutlet weak var periodButton: UIButton!
 
@@ -43,5 +43,27 @@ class ExpensesHeaderView: UIView {
             options:NSLayoutFormatOptions(rawValue: 0),
             metrics:nil,
             views: bindings))
+    }
+    
+    func adaptivePresentationStyleForPresentationController(controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .None
+    }
+
+    @IBAction func tapButton(sender: AnyObject) {
+        //TODO あとでViewを作る
+        let vc: UIViewController = UIViewController()
+        vc.modalPresentationStyle = .Popover
+        vc.preferredContentSize = CGSize(width: 100, height: 100)
+        vc.popoverPresentationController?.backgroundColor = UIColor.blueColor()
+        vc.popoverPresentationController?.delegate = self
+        vc.popoverPresentationController?.sourceView = sender as? UIView
+        vc.popoverPresentationController?.sourceRect = sender.bounds
+        vc.popoverPresentationController?.permittedArrowDirections = .Up
+
+        var root = UIApplication.sharedApplication().keyWindow?.rootViewController
+        while let present = root?.presentedViewController {
+            root = present
+        }
+        root!.presentViewController(vc, animated: true, completion: nil)
     }
 }
