52314b68a4033f4774043cf555bbbcbeda06a18a
Yuki Sumida <y.sumida.0201@gmail.com>
2017-01-21 11:47:55 +0900

    交通機関選択用のセル仮実装

---
+++ myExpenses/TransportSelectCell.swift
@@ -0,0 +1,26 @@
+//
+//  TransportSelectCell.swift
+//  myExpenses
+//
+//  Created by Yuki Sumida on 2017/01/21.
+//
+//
+
+import UIKit
+
+class TransportSelectCell: UITableViewCell {
+    @IBOutlet weak var transportName: UILabel!
+    @IBOutlet weak var transportSwitch: UISwitch!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
