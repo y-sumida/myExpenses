539103f96c51d91fe858a14b299584910a7a0307
Yuki Sumida <y.sumida.0201@gmail.com>
2017-01-31 22:56:31 +0900

    Stringのextensionで空文字以外でtrueを返すパラメータ追加

---
+++ myExpenses/StringExtension.swift
@@ -0,0 +1,13 @@
+//
+//  StringExtension.swift
+//  myExpenses
+//
+//  Created by Yuki Sumida on 2017/01/31.
+//
+//
+
+extension String {
+    var isNotEmpty: Bool {
+        return !self.isEmpty
+    }
+}
