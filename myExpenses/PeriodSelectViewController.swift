fc3d056c04d35e730fcdd02e3a463f5ec115f79f
Yuki Sumida <y.sumida.0201@gmail.com>
2017-02-04 09:07:34 +0900

    TODOコメント追加

--- myExpenses/PeriodSelectViewController.swift
+++ myExpenses/PeriodSelectViewController.swift
@@ -10,15 +10,15 @@ import UIKit
 
 class PeriodSelectViewController: UIViewController {
 
+    // TODO 表示する月の設定　過去半年分くらい？
+    // TODO UITableViewDelegate実装
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func tapButton(sender: AnyObject) {
