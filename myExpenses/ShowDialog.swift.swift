4034ef2faeb9cd0572c9ca2fab8394f084dda63f
Yuki Sumida <y.sumida.0201@gmail.com>
2017-02-15 23:27:04 +0900

    OK,キャンセルのボタンを持つダイアログ表示をプロトコル化

--- myExpenses/ShowDialog.swift.swift
+++ myExpenses/ShowDialog.swift.swift
@@ -12,6 +12,7 @@ import UIKit
 protocol ShowDialog {
     func showErrorDialog(error: APIResult, handler: ((UIAlertAction) -> Void)?)
     func showCompleteDialog(message: String, handler: ((UIAlertAction) -> Void)?)
+    func showConfirmDialog(message: String, defaultHandler: ((UIAlertAction) -> Void)?, cancelHandler: ((UIAlertAction) -> Void)?)
 }
 
 extension ShowDialog {
@@ -29,6 +30,20 @@ extension ShowDialog {
     func showCompleteDialog(message: String, handler: ((UIAlertAction) -> Void)? = nil) {
         let dialog = UIAlertController(title: message, message: "", preferredStyle: .Alert)
         dialog.addAction(UIAlertAction(title: "OK", style: .Default, handler: handler))
+        var root = UIApplication.sharedApplication().keyWindow?.rootViewController
+        while let present = root?.presentedViewController {
+            root = present
+        }
+        root!.presentViewController(dialog, animated: true, completion: nil)
+    }
+
+    func showConfirmDialog(message: String, defaultHandler: ((UIAlertAction) -> Void)?, cancelHandler: ((UIAlertAction) -> Void)?) {
+        let dialog: UIAlertController = UIAlertController(title: message, message: "", preferredStyle:  UIAlertControllerStyle.Alert)
+        let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: defaultHandler)
+        let cancelAction: UIAlertAction = UIAlertAction(title: "キャンセル", style: UIAlertActionStyle.Cancel, handler: cancelHandler)
+        dialog.addAction(cancelAction)
+        dialog.addAction(defaultAction)
+
         var root = UIApplication.sharedApplication().keyWindow?.rootViewController
         while let present = root?.presentedViewController {
             root = present
