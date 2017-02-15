22c545aa541da23c11910d54707f638ddfa39805
Yuki Sumida <y.sumida.0201@gmail.com>
2017-02-15 23:52:47 +0900

    ダイアログ表示プロトコルのrootViewをプロパティ化

--- myExpenses/ShowDialog.swift.swift
+++ myExpenses/ShowDialog.swift.swift
@@ -10,44 +10,40 @@ import Foundation
 import UIKit
 
 protocol ShowDialog {
+    var root: UIViewController { get }
     func showErrorDialog(error: APIResult, handler: ((UIAlertAction) -> Void)?)
     func showCompleteDialog(message: String, handler: ((UIAlertAction) -> Void)?)
     func showConfirmDialog(message: String, defaultHandler: ((UIAlertAction) -> Void)?, cancelHandler: ((UIAlertAction) -> Void)?)
 }
 
 extension ShowDialog {
-    func showErrorDialog(error: APIResult, handler: ((UIAlertAction) -> Void)? = nil) {
-        let dialog = UIAlertController(title: error.code, message: error.message, preferredStyle: .Alert)
-        dialog.addAction(UIAlertAction(title: "OK", style: .Default, handler: handler))
-        // TODO extensionとかに抽出したい
+    var root: UIViewController {
         var root = UIApplication.sharedApplication().keyWindow?.rootViewController
         while let present = root?.presentedViewController {
             root = present
         }
-        root!.presentViewController(dialog, animated: true, completion: nil)
+        return root!
+    }
+
+    func showErrorDialog(error: APIResult, handler: ((UIAlertAction) -> Void)? = nil) {
+        let dialog = UIAlertController(title: error.code, message: error.message, preferredStyle: .Alert)
+        dialog.addAction(UIAlertAction(title: "OK", style: .Default, handler: handler))
+
+        self.root.presentViewController(dialog, animated: true, completion: nil)
     }
 
     func showCompleteDialog(message: String, handler: ((UIAlertAction) -> Void)? = nil) {
         let dialog = UIAlertController(title: message, message: "", preferredStyle: .Alert)
         dialog.addAction(UIAlertAction(title: "OK", style: .Default, handler: handler))
-        var root = UIApplication.sharedApplication().keyWindow?.rootViewController
-        while let present = root?.presentedViewController {
-            root = present
-        }
-        root!.presentViewController(dialog, animated: true, completion: nil)
+
+        self.root.presentViewController(dialog, animated: true, completion: nil)
     }
 
     func showConfirmDialog(message: String, defaultHandler: ((UIAlertAction) -> Void)?, cancelHandler: ((UIAlertAction) -> Void)?) {
         let dialog: UIAlertController = UIAlertController(title: message, message: "", preferredStyle:  UIAlertControllerStyle.Alert)
-        let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: defaultHandler)
-        let cancelAction: UIAlertAction = UIAlertAction(title: "キャンセル", style: UIAlertActionStyle.Cancel, handler: cancelHandler)
-        dialog.addAction(cancelAction)
-        dialog.addAction(defaultAction)
+        dialog.addAction(UIAlertAction(title: "OK", style: .Default, handler: defaultHandler))
+        dialog.addAction(UIAlertAction(title: "キャンセル", style: .Cancel, handler: cancelHandler))
 
-        var root = UIApplication.sharedApplication().keyWindow?.rootViewController
-        while let present = root?.presentedViewController {
-            root = present
-        }
-        root!.presentViewController(dialog, animated: true, completion: nil)
+        self.root.presentViewController(dialog, animated: true, completion: nil)
     }
 }
