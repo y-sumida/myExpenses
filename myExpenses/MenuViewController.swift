8249b6c0c66ce1f84ea686d2bd1fd401e44199c5
Yuki Sumida <y.sumida.0201@gmail.com>
2017-02-19 22:43:08 +0900

    メニュー画面の仮実装

--- myExpenses/MenuViewController.swift
+++ myExpenses/MenuViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MenuViewController: UIViewController {
+class MenuViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var table: UITableView!
 
     override func viewDidLoad() {
@@ -19,9 +19,24 @@ class MenuViewController: UIViewController {
             navi.setNavigationBarHidden(false, animated: true)
             navigationItem.title = "設定"
         }
+
+        table.delegate = self
+        table.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
