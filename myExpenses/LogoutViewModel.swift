13f169c83c84330b3232bb8ed996c1384633850e
Yuki Sumida <y.sumida.0201@gmail.com>
2017-02-21 23:06:55 +0900

    ログアウト用のビューモデル

--- myExpenses/LogoutViewModel.swift
+++ myExpenses/LogoutViewModel.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import RxSwift
+
+class LogoutViewModel {
+    private let bag: DisposeBag = DisposeBag()
+
+    var logoutTrigger: PublishSubject<Void> = PublishSubject()
+    var result: Variable<ErrorType?> = Variable(nil)
+
+    init() {
+        logoutTrigger
+            .flatMap {
+                LogoutModel.call()
+            }
+            .observeOn(MainScheduler.instance)//これ以降メインスレッドで実行
+            .subscribe(
+                onNext: { (model, response) in
+                    self.result.value = model.result!
+                },
+                onError: { (error: ErrorType) in
+                    // APIエラー
+                    self.result.value = error
+                }
+            )
+            .addDisposableTo(bag)
+    }
+}
