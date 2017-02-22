708001b10ffb7ff3da5dbc234d421054668c5079
Yuki Sumida <y.sumida.0201@gmail.com>
2017-02-22 23:47:58 +0900

    交通費登録APIでJSON不正の場合にモデルをnilになるように

--- myExpenses/PostExpenseModel.swift
+++ myExpenses/PostExpenseModel.swift
@@ -15,22 +15,20 @@ class PostExpenseModel: ResponseProtocol {
     var resultMessage: String = ""
     var sessionId: String = ""
 
-    required init(data: NSDictionary) {
-        if let resultCode = data["resultCode"] {
-            self.resultCode = resultCode as! String
-        }
+    required init?(data: NSDictionary) {
+        guard let resultCode = data["resultCode"],
+            let resultMessage = data["resultMessage"],
+            let sessionId = data["sessionId"]
+            else { return nil }
+
+        self.resultCode = resultCode as! String
+        self.resultMessage = resultMessage as! String
         
-        if let resultMessage = data["resultMessage"] {
-            self.resultMessage = resultMessage as! String
-        }
-        
-        if let sessionId = data["sessionId"] {
-            self.sessionId = sessionId as! String
-            // セッションID更新
-            let sharedInstance: NSUserDefaults = NSUserDefaults.standardUserDefaults()
-            sharedInstance.setObject(self.sessionId, forKey: "sessionId")
-            sharedInstance.synchronize()
-        }
+        self.sessionId = sessionId as! String
+        // セッションID更新
+        let sharedInstance: NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        sharedInstance.setObject(self.sessionId, forKey: "sessionId")
+        sharedInstance.synchronize()
         
         result = APIResult(code: self.resultCode, message: self.resultMessage, sessionId: self.sessionId)
     }
