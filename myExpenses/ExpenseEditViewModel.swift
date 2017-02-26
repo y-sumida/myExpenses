43a53c48a422fe70d97b7d1480ff3bcfb937df26
Yuki Sumida <y.sumida.0201@gmail.com>
2017-02-26 21:34:11 +0900

    交通費編集用ビューモデルの強参照を修正

--- myExpenses/ExpenseEditViewModel.swift
+++ myExpenses/ExpenseEditViewModel.swift
@@ -54,15 +54,17 @@ class ExpenseEditViewModel {
         self.useBus.value = expense.useBus
         self.useHighway.value = expense.useHighway
     }
-    
+
     func upsertExpense() {
         PostExpenseModel.call(self.expense)
             .observeOn(MainScheduler.instance)//これ以降メインスレッドで実行
             .subscribe(
-                onNext: { model, response in
+                onNext: { [weak self] model, response in
+                    guard let `self` = self else { return }
                     self.result.value = model.result
                 },
-                onError: { (error: ErrorType) in
+                onError: { [weak self] (error: ErrorType) in
+                    guard let `self` = self else { return }
                     // APIエラー
                     self.result.value = error
                 }
