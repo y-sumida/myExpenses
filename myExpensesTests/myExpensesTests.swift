a1bf0360f3cc373059cffa76f8bc38679c70e221
Yuki Sumida <y.sumida.0201@gmail.com>
2017-03-18 22:42:58 +0900

    テストの準備

---
+++ myExpensesTests/myExpensesTests.swift
@@ -0,0 +1,34 @@
+//
+//  myExpensesTests.swift
+//  myExpensesTests
+//
+//  Created by Yuki Sumida on 2017/03/18.
+//
+//
+
+import XCTest
+
+class myExpensesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+       XCTAssertFalse("".isNotEmpty)
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
