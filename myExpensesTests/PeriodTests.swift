029379610b68847ea1287043f016548e9e2baa01
Yuki Sumida <y.sumida.0201@gmail.com>
2017-03-19 22:04:14 +0900

    Period型のテスト追加

--- myExpensesTests/PeriodTests.swift
+++ myExpensesTests/PeriodTests.swift
@@ -50,7 +50,7 @@ class PeriodTests: XCTestCase {
 
     func test日付上限() {
         let formatter = NSDateFormatter()
-        formatter.dateFormat = "yyyyMMdd hh:mm:ss"
+        formatter.dateFormat = "yyyyMMdd HH:mm:ss"
         let period: Period?
         if let date: NSDate = formatter.dateFromString("20170519 23:59:59") {
             period = Period(date: date)
@@ -60,5 +60,22 @@ class PeriodTests: XCTestCase {
         }
 
         XCTAssertEqual(period?.description, "201705")
+    }
+
+    func test過去半年() {
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyyMMdd HH:mm:ss"
+        let periods: [Period]?
+        if let date: NSDate = formatter.dateFromString("20170519 23:59:59") {
+            periods = Period.pastHalfYear(date)
+        }
+        else {
+            periods = Period.pastHalfYear()
+        }
+
+        let expected = ["201705", "201704", "201703", "201702", "201701", "201612"]
+        let actual = periods?.map { $0.description}
+
+        XCTAssertEqual(actual!, expected)
     }
 }
