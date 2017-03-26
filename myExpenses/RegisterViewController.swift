b54f8f35ff57c9fcca8edc11c8ad2136ac9d0908
Yuki Sumida <y.sumida.0201@gmail.com>
2017-03-26 13:36:20 +0900

    新規登録画面の入力フォームをすべて入力するまでボタンdisable

--- myExpenses/RegisterViewController.swift
+++ myExpenses/RegisterViewController.swift
@@ -7,8 +7,15 @@
 //
 
 import UIKit
+import RxSwift
 
 class RegisterViewController: UIViewController {
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var passwordCheck: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
+
+    private let bag: DisposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +25,17 @@ class RegisterViewController: UIViewController {
             navi.setNavigationBarHidden(false, animated: true)
             navigationItem.title = "ユーザー登録"
         }
+
+        // 全部入力するまでボタンdisable
+        Observable
+            .combineLatest(
+                email.rx.text.asObservable(),
+            password.rx.text.asObservable(),
+            passwordCheck.rx.text.asObservable()) { c in c }
+            .bindNext { [unowned self] (email, password, passwordCheck) in
+                self.registerButton.isEnabled = (email?.isNotEmpty)! && (password?.isNotEmpty)! && (passwordCheck?.isNotEmpty)!
+            }
+            .addDisposableTo(bag)
     }
 
     override func didReceiveMemoryWarning() {
